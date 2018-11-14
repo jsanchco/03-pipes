fd6a2184adb84f5b30dea3952aad988724fccba6
jsanchco-WORK <jsanchez@slingtechnology.net>
2018-11-14 19:49:42 +0100

    jsanchez-WORK. Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre = "Jesús";
+  numbers = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  a = 0.234;
+  salario = 1234.5;
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Calle XXX',
+      numero: 45
+    }
+  };
+  valorPromesa = new Promise (( resolve, reject ) => {
+    setTimeout( ()=>resolve('Llegó la data!!!'), 3500 );
+  });
+  fecha = new Date();
+
 }
